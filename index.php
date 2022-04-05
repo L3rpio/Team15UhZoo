442b5f81f31503cc540d3fe3aacb77d1ed098174
L3rpio <leroyphan39@gmail.com>
2022-04-04 19:22:58 -0500

    add openconnection to see if site would work

--- index.php
+++ index.php
@@ -10,6 +10,29 @@
     <h1>Hello guys!</h1>
     <p>this should work but idk why it's not working</p>
     <?php 
+      function ReadData() {
+        try
+          {
+            $conn = OpenConnection();
+            $tsql = "SELECT [CompanyName] FROM SalesLT.Customer";
+            $getProducts = sqlsrv_query($conn, $tsql);
+            if ($getProducts == FALSE)
+              die(FormatErrors(sqlsrv_errors()));
+            $productCount = 0;
+            while($row = sqlsrv_fetch_array($getProducts, SQLSRV_FETCH_ASSOC))
+            {
+              echo($row['CompanyName']);
+              echo("<br/>");
+              $productCount++;
+            }
+            sqlsrv_free_stmt($getProducts);
+            sqlsrv_close($conn);
+          }
+          catch(Exception $e)
+          {
+            echo("Error!");
+          }
+      }
       echo("hello world");
     ?>
   </body>
