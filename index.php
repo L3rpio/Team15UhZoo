1b8ca3ea09d1bf5c26860fa9fbf03f03957e84a2
L3rpio <leroyphan39@gmail.com>
2022-04-04 19:30:27 -0500

    calling actual openconnection()

--- index.php
+++ index.php
@@ -16,9 +16,11 @@
           $connectionOptions = array("Database"=>"UH_Zoo_Database",
               "Uid"=>"dhphan3", "PWD"=>"K7EY2kh@ri*oJH9");
           $conn = sqlsrv_connect($serverName, $connectionOptions);
-          if($conn == false)
-              die(FormatErrors(sqlsrv_errors()));
-  
+          if($conn == false){
+            die(FormatErrors(sqlsrv_errors()));
+          } else {
+            echo("Connection made");
+          }
           return $conn;
       }
     ?>
