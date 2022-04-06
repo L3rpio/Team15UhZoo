8998e0fc6a2883ab74f8516589b9dcfc2e064def
zondar01 <94730719+zondar01@users.noreply.github.com>
2022-04-06 18:47:12 -0500

    test2 of connection

--- customer_crud2.php
+++ customer_crud2.php
@@ -1,13 +1,19 @@
-<?php
-$serverName = "tcp:cosc3380-zoo.database.windows.net,1433";
-$connectionOptions = array("Database"=>"ZooDB",
-	"Uid"=>"dhphan3", "PWD"=>"MyPassword");
-$conn = sqlsrv_connect($serverName, $connectionOptions);
-if($conn){
-	echo"Connection establsihed.<br />";
-}
-else{
-	echo "Connection could not be established.<br />";
-     die( print_r( sqlsrv_errors(), true));
-}
-?>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <?php
+        $serverName = "tcp:cosc3380-zoo.database.windows.net,1433";
+        $connectionOptions = array("Database"=>"ZooDB",
+            "Uid"=>"dhphan3", "PWD"=>"MyPassword");
+        $conn = sqlsrv_connect($serverName, $connectionOptions);
+        if($conn){
+            echo"Connection establsihed.<br />";
+        }
+        else{
+            echo "Connection could not be established.<br />";
+            die( print_r( sqlsrv_errors(), true));
+        }
+        ?>
+    </head>
+
+</html>
