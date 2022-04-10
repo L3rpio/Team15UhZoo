603b302fddfd3add00b1747edf4b42e1ae7afe62
zondar01 <94730719+zondar01@users.noreply.github.com>
2022-04-10 16:39:14 -0500

    select database query

--- fml.php
+++ fml.php
@@ -19,21 +19,21 @@
       }
       echo "Connected succesfully";
       
-      $sql = "USE uh_zoo;";
+      $result = $conn->query("SELECT DATABASE()");
+      $row = $result->fetch_row();
+      printf("Default database is %s.\n", $row[0]);
+//       mysql_select_db('uh_zoo') or die(mysql_error());
+// ;     $sql="SELECT * FROM Customer;";
+//       $result = mysqli_query($conn, $sql);
 
-      
-      $result = mysqli_query($conn, $sql);
-      $sql="SELECT * FROM Customer;";
-      $result = mysqli_query($conn, $sql);
-
-      if (mysqli_num_rows($result) > 0) {
-      // output data of each row
-        while($row = mysqli_fetch_assoc($result)) {
-          echo $row[0];
-          }
-      } else {
-        echo "0 results";
-      }
+//       if (mysqli_num_rows($result) > 0) {
+//       // output data of each row
+//         while($row = mysqli_fetch_assoc($result)) {
+//           echo $row[0];
+//           }
+//       } else {
+//         echo "0 results";
+//       }
     }
     OpenConnection();
   ?>
