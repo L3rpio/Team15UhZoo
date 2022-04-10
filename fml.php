155c86bed5a1dfee7a1b5b92f413a151ef5d0b46
zondar01 <94730719+zondar01@users.noreply.github.com>
2022-04-10 15:42:43 -0500

    new sql db

--- fml.php
+++ fml.php
@@ -2,41 +2,25 @@
 <?php
   function OpenConnection()
   {
-      $serverName = "cosc3380-zoo.database.windows.net";
-      $connectionOptions = array("Database"=>"ZooDatabaseDump",
-          "Uid"=>"dhphan3", "PWD"=>"K7EY2kh@ri*oJH9");
-      $conn = sqlsrv_connect($serverName, $connectionOptions);
+      $serverName = "uh-zoo-db.mysql.database.azure.com";
+      $username ="zooadmin";
+      $password= "Ab!2Xui5efd3!L&";
+      $conn = new mysqli($serverName, $username, $password);
       if($conn == false){
-        echo("Connection could not be established");
-        die(FormatErrors(sqlsrv_errors()));
+        die("Connection failed: " . $conn->connect_error);
       }
-      echo("Connection made if we made it at this point");
+      echo "Connected succesfully";
       return $conn;
   }
+  OpenConnection();
   ?>
 <html lang="en">
   <head>
     <title>testing queries</title>
   </head>
   <body>
-    hi there 9 <br>
-    <?php
-      //OpenConnection();
-       $conn = OpenConnection();
-       $tsql = "SELECT * FROM dbo.Customer;";
-       $getProducts = sqlsrv_query($conn, $tsql);
-       echo("were not retrieving the table idk why fml");
-       //$resultCheck=mysqli_num_rows($getProducts);
-       //$row = mysqli_fetch_assoc($result);
-       //echo $row['Customer_First_Name'] . "<br>";
-       //if ($resultCheck > 0){
-         //echo("entered the if statement");
-        // while ($row = mysqli_fetch_assoc($result)){
-          // echo $row['Customer_First_Name'] . "<br>";
-        //}
-       }
-      ?>
     
-    howdy changed commented while loop and if <br>
+    howdy <br>
+
   </body>
 </html>
