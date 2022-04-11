7b933c9282d54ea55537d35eaa9eace4fd39f566
Duong Phan <38167242+L3rpio@users.noreply.github.com>
2022-04-11 13:31:18 -0500

    created managercrud.php
    This PHP file fetches the employees' data for the manager.php page to display. For now, it fetches all employees instead of employees working under a certain manager.

---
+++ managercrud.php
@@ -0,0 +1,17 @@
+<?php 
+   $serverName = "zoodbteam15-server.mysql.database.azure.com";
+   $username ="zooadmin";
+   $password= "Lovec++123";
+   $conn = new mysqli($serverName, $username, $password,"uh_zoo");
+   if($conn == false){
+     die("Connection failed: " . $conn->connect_error);
+   }
+
+   $sql = "select * from employee";
+   $result = mysqli_query($conn, $sql);
+   $employees = mysqli_fetch_all($result, MYSQLI_ASSOC);
+
+
+   mysqli_close($conn);
+   die(mysqli_error($conn));
+?>
