f6c9c4665c181d94242cc4a9dec94a7fcd992fb4
zondar01 <94730719+zondar01@users.noreply.github.com>
2022-04-10 20:26:53 -0500

    only first name output

--- fml.php
+++ fml.php
@@ -19,7 +19,6 @@
       }
       mysqli_free_result($result);
       mysqli_close($conn);
-      print_r($pizzas);
       // if (mysqli_num_rows($result) > 0) {
       // // output data of each row
       //   while($row = mysqli_fetch_assoc($result)) {
