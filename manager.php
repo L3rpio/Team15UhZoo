3451847f2e8c7cac7a2a8bcdcd47fbc62f86b68b
Duong Phan <38167242+L3rpio@users.noreply.github.com>
2022-04-11 13:51:53 -0500

    Update manager.php

--- manager.php
+++ manager.php
@@ -102,6 +102,40 @@
                   echo "<td>" . $wage . "</td>";
                   echo "<td>" . $hoursWorked . "</td>";
                 ?>
+                  <a href="#editEmployeeModal" class="edit" data-toggle="modal"
+                    ><i
+                      class="material-icons"
+                      data-toggle="tooltip"
+                      title="Edit"
+                      >&#xE254;</i
+                    ></a
+                  >
+                  <a
+                    href="#deleteEmployeeModal"
+                    class="delete"
+                    data-toggle="modal"
+                    ><i
+                      class="material-icons"
+                      data-toggle="tooltip"
+                      title="Delete"
+                      >&#xE872;</i
+                    ></a
+                  >
+                </td>
+              </tr>
+              <?php } ?>
+              <tr>
+                <td>
+                  <span class="custom-checkbox">
+                    <input
+                      type="checkbox"
+                      id="checkbox1"
+                      name="options[]"
+                      value="1"
+                    />
+                    <label for="checkbox1"></label>
+                  </span>
+                </td>
                 <td>Thomas Hardy</td>
                 <td>thomashardy@mail.com</td>
                 <td>89 Chiaroscuro Rd, Portland, USA</td>
@@ -128,7 +162,6 @@
                   >
                 </td>
               </tr>
-              <?php } ?>
             </tbody>
           </table>
           <div class="clearfix">
