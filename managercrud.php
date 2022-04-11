76566769b79fe4f29f3faf72cf0cbb52eaf351ec
Duong Phan <38167242+L3rpio@users.noreply.github.com>
2022-04-11 13:44:41 -0500

    Update managercrud.php

--- managercrud.php
+++ managercrud.php
@@ -5,14 +5,15 @@
    $conn = new mysqli($serverName, $username, $password,"uh_zoo");
    if($conn == false){
      die("Connection failed: " . $conn->connect_error);
+   } else {
+      echo "connection success!"
    }
 
    $sql = "select * from employee";
    $result = mysqli_query($conn, $sql);
    $employees = mysqli_fetch_all($result, MYSQLI_ASSOC);
 
-   
-   echo "hello there";
+   print_r($employees);
 
    mysqli_close($conn);
    die(mysqli_error($conn));
