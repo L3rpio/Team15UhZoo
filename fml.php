6d37faf8aefcd538c17646e9f6e3ff72a8bb0498
zondar01 <94730719+zondar01@users.noreply.github.com>
2022-04-10 19:27:47 -0500

    no echo

--- fml.php
+++ fml.php
@@ -1,30 +1,45 @@
 <!DOCTYPE html>
-<?php
-  $serverName = "uh-zoo-db.mysql.database.azure.com";
-  $username ="zooadmin";
-  $password= "Ab!2Xui5efd3!L&";
-  $db_name = 'uh_zoo';
-  $conn = mysqli_connect($serverName, $username, $password, $db_name);
-  $sql = "select * from customer";
-
-  $query = $conn->query($sql);
-
-  if($query->num_rows > 0){
-    echo "success";
-  } else {
-    echo "0 results";
-  }
-?>
 <html lang="en">
 <head>
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Testing Queries</title>
+  <title>Document</title>
 </head>
 <body>
-  <h1>Hello world. This is where we'll be testing our queries</h1>
-  
+<?php
+  function OpenConnection()
+  {
+      $serverName = "uh-zoo-db.mysql.database.azure.com";
+      $username ="zooadmin";
+      $password= "Ab!2Xui5efd3!L&";
+      $conn = new mysqli($serverName, $username, $password,'uh_zoo');
+      if($conn == false){
+        die("Connection failed: " . $conn->connect_error);
+      }
+      
+      //$result = $conn->query("SELECT DATABASE('uh_zoo)");
+      //$row = $result->fetch_row();
+      //echo $row[0];
+      //mysql_select_db("uh_zoo", $conn);
+      $sql="SELECT * FROM Customer";
+      $result = mysqli_query($conn, $sql);
+      $pizzas=mysqli_fetch_all($result, MYSQLI_ASSOC);
+      mysqli_free_result($result);
+      mysqli_close($conn);
+      print_r($pizzas);
+      // if (mysqli_num_rows($result) > 0) {
+      // // output data of each row
+      //   while($row = mysqli_fetch_assoc($result)) {
+      //     echo $row[0];
+      //     }
+      // } else {
+      //   echo "0 results";
+      // }
+      die(mysqli_error($conn));
+    }
+    OpenConnection();
+  ?>
 </body>
 </html>
 
