6dad1a8cd4e501c948178df9624b84a67a04596b
zondar01 <94730719+zondar01@users.noreply.github.com>
2022-04-10 19:40:56 -0500

    just php

--- fml.php
+++ fml.php
@@ -32,17 +32,5 @@
       // }
       die(mysqli_error($conn));
     }
-    OpenConnection();
   ?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
-</head>
-<body>
-</body>
-</html>
 
