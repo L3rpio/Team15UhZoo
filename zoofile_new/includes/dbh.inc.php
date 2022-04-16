e7a97a768643f6978bb34c51d3d609ef4cb3fed6
WatadOmar <98974026+WatadOmar@users.noreply.github.com>
2022-04-16 08:30:22 +0300

    Add files via upload

---
+++ zoofile_new/includes/dbh.inc.php
@@ -0,0 +1,24 @@
+<?php
+//This script logs you into the database
+
+
+
+$serverName = "zoodbteam15-server.mysql.database.azure.com";
+$username = "zooadmin";
+$password = "Lovec++123";
+$database = "uh_zoo";
+
+// $serverName = "localhost";
+// $username = "root";
+// $password = "";
+// $database = "zooadmin";
+
+
+$conn = new mysqli($serverName, $username, $password, $database);
+if ($conn == false) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+/*else {
+echo("Connection made");
+}*/
