5f9e6c955d1b79407edc7b1a95016b9bb18dcb03
WatadOmar <98974026+WatadOmar@users.noreply.github.com>
2022-04-16 09:01:23 +0300

    for signing up
    for sending email when signing up

--- includes/signup.inc.php
+++ includes/signup.inc.php
@@ -1,50 +1,52 @@
 <?php
 
-ob_start();
-if(isset($_POST["submit"]))
-{
-    $email = $_POST["email"];
-    $fname = $_POST["fname"];
-    $lname = $_POST["lname"];
-    $user = $_POST["username"];
-    $pass = $_POST["password"];
-    $pwdrepeat = $_POST["passwordrepeat"];
+// ob_start();
+if (isset($_POST["submit"])) {
+// $email = $_POST["email"];
+    $fname     = $_POST["fname"];
+    $lname     = $_POST["lname"];
+    $user      = $_POST["username"];
+    $email     = $_POST["email"];
+    $pass      = base64_encode($_POST["password"]);
+    $pwdrepeat = base64_encode($_POST["passwordrepeat"]);
 
     require_once 'dbh.inc.php';
     require_once 'functions.inc.php';
 
-     if(emptyInputSignup($email, $fname, $lname,$user, $pass,$pwdrepeat) !== false){
-    //if(emptyInputSignup($fname, $lname,$user, $pass,$pwdrepeat) !== false){
+// if(emptyInputSignup($email, $fname, $lname,$user, $pass,$pwdrepeat) !== false){
+    if (emptyInputSignup($fname, $lname, $user, $pass, $pwdrepeat) !== false) {
         header("location:../GuestSignUp.php?error=emptyinput");
-        die();
         exit();
     }
-    if(invalidUID($user) !== false){
+    if (invalidUID($user) !== false) {
         header("location:../GuestSignUp.php?error=invalidUID");
-        die();
         exit();
     }
 
-    if(mismatchpassword($pass, $pwdrepeat) !== false){
+    if (mismatchpassword($pass, $pwdrepeat) !== false) {
         header("location:../GuestSignUp.php?error=pwdmissmatch");
-        die();
         exit();
     }
-    if(usertaken($conn, $user) !== false){
+    if (usertaken($conn, $user) !== false) {
         header("location:../GuestSignUp.php?error=useralreadyexists");
-        die();
         exit();
     }
-    createUser2($conn, $fname, $lname, $user, $pass, $email);
-}
-else{
+    createUser2($conn, $fname, $lname, $user, $email, $pass);
+
+    /*$insert = createUser2($conn, $fname, $lname, $user, $pass);
+if ($insert == true) {
+
+echo "Data insertion success!";
+} else {
+echo "Insertion Error!";
+}*/
+} else {
     header("location:/..GuestSignUp.php");
-    die();
     exit();
 }
-ob_end_clean();
+// ob_end_clean();
 
-    // if(bademail($email) !== false){
-    //     header("location:../GuestSignUp.php?error=bademail");
-    //     exit();
-    // }
+// if(bademail($email) !== false){
+//     header("location:../GuestSignUp.php?error=bademail");
+//     exit();
+// }
