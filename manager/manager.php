3748f39573a268cd5431d9981ad8429b8490cbe9
Duong Phan <38167242+L3rpio@users.noreply.github.com>
2022-04-17 18:26:40 -0500

    finished up ticket.php

--- manager/manager.php
+++ manager/manager.php
@@ -185,7 +185,7 @@
           <div class="table-title">
             <div class="row">
               <div class="col-sm-6">
-                <h2>Manage Employees</h2>
+                <h2>Manage Employees at <?php echo $workPlaceName; ?></h2>
               </div>
               <div class="col-sm-6">
                 <a
--- ticket/ticketProcessing.php
+++ ticket/ticketProcessing.php
@@ -8,12 +8,60 @@ if($conn == false){
   die("Connection failed: " . $conn->connect_error);
 }
 
+function validateDate($date){
+  $year = intval(substr($date, 0, 4));
+  $month = intval(substr($date, 5, 7));
+  $day = intval(substr($date, 8));
+
+  $currDate = date('Y-m-d');
+  $currYear = intval(substr($currDate, 0, 4));
+  $currMonth = intval(substr($currDate, 5, 7));
+  $currDay = intval(substr($currDate, 8));
+
+  if($year < $currYear || $month < $currMonth || $day <= $currDay){
+    header("location: ticket.php?error=invaliddate");
+    exit();
+  }
+}
+
+function validateTime($time){
+  $hour = substr($time, 0, 2);
+  $minute = substr($time, 2);
+
+  if($hour < 10 || $hour > 19){
+     header("location: ticket.php?error=invalidtime");
+    echo $hour;
+    exit();
+  }
+}
+
+function validateEmail($email){
+  if(!filter_var($email, FILTER_VALIDATE_EMAIL)){
+    header("location: ticket.php?error=invalidemail");
+    exit();
+  }
+}
+
+function validateAge($age){
+  $age = intval($age);
+  if($age < 0){
+    header("location: ticket.php?error=invalidage");
+    exit();
+  }
+}
+
 if(isset($_POST['reserveticket'])){
-  echo  print_r($_POST, true);
   $visitDate = $_POST["visitdate"];
   $visitTime = $_POST["visittime"]; 
   $email = $_POST['email'];
   $age = $_POST['age'];
+  // $user_id = $_SESSION['id'];
+
+  validateDate($visitDate);
+  validateTime($visitTime);
+  validateEmail($email);
+  validateAge($age);
+
   if($age >= 5 && $age <= 12){
     $cost = 10;
   } else if($age >= 13 && $age <= 149){
@@ -25,7 +73,7 @@ if(isset($_POST['reserveticket'])){
   (date_of_purchase, date_of_visit, cost, bought_by, visit_time) 
   values('$visitDate', CURDATE(), $cost, 1, '$visitTime');";  // change 1 to the customer id
   $run = mysqli_query($conn, $reserveTicketQuery);
-  echo "ticket reserved";  // eventually redirect to customer portal
+  header('location: ticket.php?status=reserved');
 }
 
 mysqli_close($conn);
