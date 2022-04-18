49ba1bb0dfb9c5b7318958eaeadd9cdc01e9aab9
WatadOmar <98974026+WatadOmar@users.noreply.github.com>
2022-04-18 12:15:41 +0300

    Add files via upload

--- GuestLanding.php
+++ GuestLanding.php
@@ -1,7 +1,8 @@
 <?php
 session_start();
-if (!isset($_SESSION['id'])) {
-    header('location:LoginPage.php');
+if (!isset($_SESSION['id'])) 
+{
+header('location:LoginPage.php');
 };
 ?>
 
@@ -9,73 +10,62 @@ if (!isset($_SESSION['id'])) {
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>UH Zoo: Guest Landing</title>
-    <link rel="stylesheet" href="EmployeePortal/css/style.css">
+<meta charset="UTF-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>UH Zoo: Guest Landing</title>
+<link rel="stylesheet" href="EmployeePortal/css/style.css">
 </head>
 
 <body>
-    <nav class="navbar">
-        <span class="logo"><a href="GuestLanding.php" class="styledLink">UH Zoo</a></span>
-        <ul class="navlist">
-            <li class="listItem">
-                <a href="GuestProfileEdit.php" class="styledLink">Update Profile</a>
-            </li>
-            <li class="listItem">
-                <a href="includes/logout.inc.php" class="nav-logout">Logout</a>
-            </li>
-        </ul>
-    </nav>
-    <div class="container">
+<nav class="navbar">
+<span class="logo"><a href="GuestLanding.php" class="styledLink">UH Zoo</a></span>
+<ul class="navlist">
+<li class="listItem">
+<a href="GuestProfileEdit.php" class="nav-btn">Update Profile</a>
+</li>
+<li class="listItem">
+<a href="includes/logout.inc.php" class="nav-logout">Logout</a>
+</li>
+</ul>
+</nav>
+<div class="container">
 
-        <div class="profile">
-            <?php
-            // $select = mysqli_query($conn, "SELECT * FROM `employee` WHERE employee_id = '$user_id'") or die('query failed');
-            // if (mysqli_num_rows($select) > 0) {
-            //     $fetch = mysqli_fetch_assoc($select);
-            // }
-            // if ($fetch['image'] == '') {
-            //     echo '<img src="images\defaultUser.jpg">';
-            // } else {
-            //     echo '<img src="uploaded_img/' . $fetch['image'] . '">';
-            // }
+<div class="profile">
+<?php
+// $select = mysqli_query($conn, "SELECT * FROM `employee` WHERE employee_id = '$user_id'") or die('query failed');
+// if (mysqli_num_rows($select) > 0) {
+//     $fetch = mysqli_fetch_assoc($select);
+// }
+// if ($fetch['image'] == '') {
+//     echo '<img src="images\defaultUser.jpg">';
+// } else {
+//     echo '<img src="uploaded_img/' . $fetch['image'] . '">';
+// }
 
 
-            ?>
-            <h3>Welcome, <?php echo $_SESSION['first_name']; ?>!</h3>
-            <div class="dashboard">
-                <table class="home-table">
-                    <thead>
-                    </thead>
-                    <tbody>
-                        <tr>
-                            <td>
-                                <p>
-                                    <a href="ticket/ticket.php" class="">Click here to go to your tickets page!</a>
-                                </p>
-                            </td>
-                            <td>
-                                <p>
+?>
+<h3>Welcome, <?php echo $_SESSION['first_name']; ?>!</h3>
+<div class="dashboard">
+<table class="home-table">
+<thead>
+</thead>
+<tbody>
+<tr>
+<td>
+<p>
+<a href="" class="">
+Click here to go to your tickets page (Or, you can embed the tickets page in this page)</a>
+</p>
+</td>
 
-                                </p>
-                            </td>
-                            <td>
-                                <p>
+</tr>
+</tbody>
+</table><center><a href="product.php"><button class="btn btn-success">Pre Order Food</button></a></center> 
+</div>
+</div>
 
-                                </p>
-                            </td>
-                            <td>
-
-                            </td>
-                        </tr>
-                    </tbody>
-                </table>
-            </div>
-        </div>
-
-    </div>
+</div>
 
 </body>
 
