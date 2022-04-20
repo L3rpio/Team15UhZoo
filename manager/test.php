312fedd65f6bdc8ca7f3bd2a2ab812309c166972
Duong Phan <38167242+L3rpio@users.noreply.github.com>
2022-04-20 02:32:14 -0500

    final push of the night. will continue tomorrow

--- manager/test.php
+++ manager/test.php
@@ -1,5 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  <?php ?>
+  <form action="test.php" method="POST">
+    <input type="text" name="test" value="testing">
+    <button type="submit" name="submittingTest" value="Save Test">Save</button>
+  </form>
+
+</body>
+</html>
+
 <?php 
-session_start();
-print_r($_SESSION);
+if(isset($_POST['submittingTest'])){
+  $value = $_POST['test'];
+  echo $value;
+}
 
 ?>
