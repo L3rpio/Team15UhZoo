2d8df8227908d1b527a2945ee096cc96517c6eef
Duong Phan <38167242+L3rpio@users.noreply.github.com>
2022-04-21 00:25:56 -0500

    displaying table for expenses on manager.php. still have not fixed problem with redirection from process.php

--- manager/manager.php
+++ manager/manager.php
@@ -367,6 +367,157 @@ print_r($_SESSION);
          </div>
       </div>
 
+      <!-- html code to display expenses table for this work place -->
+      <div class="container-xl">
+         <div class="table-responsive">
+            <div class="table-wrapper">
+               <div class="table-title">
+                  <div class="row">
+                     <div class="col-sm-6">
+                        <h2>Manage Expenses</h2>
+                     </div>
+                     <div class="col-sm-6">
+                        <a
+                           href="#addExpenseModal"
+                           class="btn btn-success"
+                           data-toggle="modal"
+                           ><i class="material-icons">&#xE147;</i>
+                        <span>Add New Expense</span></a>
+                     </div>
+                  </div>
+               </div>
+               <table class="table table-striped table-hover">
+                  <thead>
+                     <tr>
+                        <th>Expense ID</th>
+                        <th>Expense Name</th>
+                        <th>Description of Expense</th>
+                        <th>Date Expense Accrued</th>
+                        <th>Expense Amount</th>
+                        <th>Action</th>
+                     </tr>
+                  </thead>
+                  <tbody>
+                     <?php 
+                        // $getExpenseIDSQL = "select * from workplace where workplace_id=$managerWorkPlaceID";
+                        // $getExpenseIDResult = mysqli_query($conn, $getExpenseIDSQL);
+                        // $expense = mysqli_fetch_all($getExpenseIDResult, MYSQLI_ASSOC);
+                        // $expenseID = $expense[0]['expense'];
+                        $getExpensesSQL = "select * from expense where expense_madeby=$managerWorkPlaceID";
+                        $getExpensesResult = mysqli_query($conn, $getExpensesSQL);
+                        $expenses = mysqli_fetch_all($getExpensesResult, MYSQLI_ASSOC);
+                        
+                        foreach($expenses as $expense){
+                          $expenseID = $expense['expense_id'];
+                          $expenseName = $expense['expense_name'];
+                          $expenseDescrip = $expense['expense_descrip'];
+                          $expenseDate = $expense['date_expense_accrued'];
+                          $expenseAmt = $expense['expense_amt'];
+                        ?>
+                     <tr>
+                        <?php 
+                           echo "<td>$expenseID</td>";
+                           echo "<td>$expenseName</td>";
+                           echo "<td>$expenseDescrip</td>";
+                           echo "<td>$expenseDate</td>";
+                           echo "<td>$$expenseAmt</td>";
+                           ?>
+                        <td>
+                           <a class="edit" href="#editExpense<?php echo $expenseID; ?>" class="btn btn-info" data-toggle="modal">
+                           <i class="material-icons" data-toggle="tooltip" title="Edit">&#xE254;</i>
+                           </a>
+                           <a class="delete" href="#deleteExpense<?php echo $expenseID; ?>" class="btn btn-danger light-link" data-toggle="modal">
+                           <i class="material-icons" data-toggle="tooltip" title="Delete">&#xE872;</i>
+                           </a>
+                        </td>
+                     </tr>
+                     <div id="editExpense<?php echo $expenseID; ?>" class="modal fade">
+                        <div class="modal-dialog">
+                           <div class="modal-content">
+                              <form action="process.php" method="post">
+                                 <div class="modal-header">
+                                    <h4 class="modal-title">Edit Employee</h4>
+                                    <button
+                                       type="button"
+                                       class="close"
+                                       data-dismiss="modal"
+                                       aria-hidden="true"
+                                       >
+                                    &times;
+                                    </button>
+                                 </div>
+                                 <div class="modal-body">
+                                    <div class="form-group hidden">
+                                       <label>ID</label>
+                                       <input type="number" name="expenseid" value="<?php echo $expenseID; ?>" class="form-control" required />
+                                    </div>
+                                    <div class="form-group">
+                                       <label>Expense Name</label>
+                                       <input type="text" name="expensename" value="<?php echo $expenseName; ?>" class="form-control" required />
+                                    </div>
+                                    <div class="form-group">
+                                       <label>Expense Description</label>
+                                       <input type="text" name="expensedescription" value="<?php echo $expenseDescrip; ?>" class="form-control" required />
+                                    </div>
+                                    <div class="form-group">
+                                       <label>Expense Amount</label>
+                                       <input type="number" name="expenseamount" value="<?php echo $expenseAmt; ?>" class="form-control" required />
+                                    </div>
+                                 </div>
+                                 <div class="modal-footer">
+                                    <input
+                                       type="button"
+                                       class="btn btn-default"
+                                       data-dismiss="modal"
+                                       value="Cancel"
+                                       />
+                                    <input type="submit" name="updateexpense" class="btn btn-info" value="Save" />
+                                 </div>
+                              </form>
+                           </div>
+                        </div>
+                     </div>
+                     <!-- Delete Modal HTML -->
+                     <div id="deleteExpense<?php echo $expenseID; ?>" class="modal fade">
+                        <div class="modal-dialog">
+                           <div class="modal-content">
+                              <form action="process.php" method="post">
+                                 <input type="number" name="expenseid" value="<?php echo $expenseID; ?>" class="form-control" required hidden/>
+                                 <div class="modal-header">
+                                    <h4 class="modal-title">Delete Expense</h4>
+                                    <button
+                                       type="button"
+                                       class="close"
+                                       data-dismiss="modal"
+                                       aria-hidden="true">
+                                    &times;
+                                    </button>
+                                 </div>
+                                 <div class="modal-body">
+                                    <p>Are you sure you want to delete this expense?</p>
+                                    <p class="text-warning">
+                                       <small>This action cannot be undone.</small>
+                                    </p>
+                                 </div>
+                                 <div class="modal-footer">
+                                    <input
+                                       type="button"
+                                       class="btn btn-default"
+                                       data-dismiss="modal"
+                                       value="Cancel"
+                                       />
+                                    <input type="submit" name="deleteexpense" class="btn btn-danger" value="Delete" />
+                                 </div>
+                              </form>
+                           </div>
+                        </div>
+                     </div>
+                     <?php } ?>
+                  </tbody>
+               </table>
+            </div>
+         </div>
+      </div>
 
    </body>
 </html>
